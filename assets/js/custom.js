1b2d2c4cdf606dc2c5126c5e34ff185012ff4897
gomi <ms960417123@gmail.com>
2022-08-08 21:07:01 +0900

    movie_chart_img_slide

---
+++ assets/js/custom.js
@@ -0,0 +1,30 @@
+//배너 이미지 슬라이드
+var mySwiper = new Swiper ('.swiper-container',{
+    navigation : {
+        nextEl : '.swiper-button-next',
+        prevEl : '.swiper-button-prev',
+    },
+    pagination : {
+        el : '.swiper-pagination',
+        type : 'bullets',
+    },
+    autoplay: {
+        delay: 4000,
+    },
+});
+
+//영화 차트 탭 메뉴
+var movBtn = $(".movie_title > ul > li");
+var movCont = $(".movie_chart > div");
+
+movCont.hide().eq(0).show();
+
+movBtn.click(function(e){
+    e.preventDefault();
+    var target = $(this);
+    var index = target.index();
+    movBtn.removeClass("active");
+    target.addClass("active");
+    movCont.css("display","none");
+    movCont.eq(index).css("display","block");
+});
